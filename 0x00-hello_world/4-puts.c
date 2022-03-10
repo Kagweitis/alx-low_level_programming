91e6ceb5593c9a7ab31828e2c73ca9472fefaa2f
Kagweitis <kagweitis@gmail.com>
2022-03-10 10:25:34 -0800

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,17 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
  *
  *  * main - main block
  *
- *   * Return: 0
+ *   * Return0
  *
  *    */
 
 int main(void)
 
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
-
 }
