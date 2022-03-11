b1fdea6890f0c2c6d0ae84ff1afe10290dcd0c4a
Kagweitis <kagweitis@gmail.com>
2022-03-11 08:42:11 -0800

    fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
  * main - main block
  * Description:  prints the alphabet in lowercase,
  * and then in uppercase, followed by a new line.
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
